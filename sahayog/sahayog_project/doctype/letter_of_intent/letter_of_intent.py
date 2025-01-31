bd27967bfeda07bb33cba34b79fa57ce4a2f89ed
arshadqureshi93 <mo.arshad.93733@gmail.com>
2025-01-31 10:54:09 +0530

    aadhar card validations

--- sahayog/sahayog_project/doctype/letter_of_intent/letter_of_intent.py
+++ sahayog/sahayog_project/doctype/letter_of_intent/letter_of_intent.py
@@ -10,15 +10,10 @@ class LetterOfIntent(Document):
 
     def validate_aadhar_number(self):
         aadhar_number = self.aadhar_number
-        
-        # Check if it has 12 digits with spaces after every 4 digits
-        if len(aadhar_number) != 14:  # 12 digits + 3 spaces
-            frappe.throw(_("Aadhar number should have 12 digits, with spaces after every 4 digits."))
-        
-        # Check if it starts with 0 or 1
-        if aadhar_number[0] in ['0', '1']:
-            frappe.throw(_("Aadhar number should not start with 0 or 1."))
-        
-        # Check if it contains only digits and spaces in the format: dddd dddd dddd
-        if not re.match(r'^\d{4} \d{4} \d{4}$', aadhar_number):
-            frappe.throw(_("Aadhar number should contain only digits and have spaces after every 4 digits."))
+
+        if not aadhar_number:
+            frappe.throw(_("Aadhar number is required."))
+
+        # Single regex check: 4 digits, space, 4 digits, space, 4 digits (not starting with 0 or 1)
+        if not re.match(r'^[2-9]\d{3} \d{4} \d{4}$', aadhar_number):
+            frappe.throw(_("Aadhar number should have 12 digits in the format 'XXXX XXXX XXXX' and should not start with 0 or 1."))
