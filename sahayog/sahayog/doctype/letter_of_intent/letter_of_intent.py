2e40a7697fe8625bddbaba14d2846200d7786f35
arshadqureshi93 <mo.arshad.93733@gmail.com>
2025-02-03 11:15:44 +0530

    feat: validate aadhar_number field in LOI

--- sahayog/sahayog/doctype/letter_of_intent/letter_of_intent.py
+++ sahayog/sahayog/doctype/letter_of_intent/letter_of_intent.py
@@ -1,9 +1,19 @@
-# Copyright (c) 2025, Developer Team and contributors
-# For license information, please see license.txt
-
+import re
 import frappe
+from frappe import _
 from frappe.model.document import Document
 
+class LetterOfIntent(Document):
+    
+    def validate(self):
+        self.validate_aadhar_number()
 
-class LetterofIntent(Document):
-	pass
+    def validate_aadhar_number(self):
+        aadhar_number = self.aadhar_number
+
+        if not aadhar_number:
+            frappe.throw(_("Aadhar number is required."))
+
+        # Single regex check: 4 digits, space, 4 digits, space, 4 digits (not starting with 0 or 1)
+        if not re.match(r'^[2-9]\d{3} \d{4} \d{4}$', aadhar_number):
+            frappe.throw(_("Aadhar number should have 12 digits in the format 'XXXX XXXX XXXX' and should not start with 0 or 1."))
