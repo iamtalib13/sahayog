b3e214f0d7edfe385568305b503cecb418b81100
arshadqureshi93 <mo.arshad.93733@gmail.com>
2025-02-03 11:22:34 +0530

    feat: validate mobile_number field in LOI

--- sahayog/sahayog/doctype/letter_of_intent/letter_of_intent.py
+++ sahayog/sahayog/doctype/letter_of_intent/letter_of_intent.py
@@ -6,14 +6,26 @@ from frappe.model.document import Document
 class LetterOfIntent(Document):
     
     def validate(self):
+        """Validates Aadhar and Mobile numbers before saving."""
         self.validate_aadhar_number()
+        self.validate_mobile_number()
 
     def validate_aadhar_number(self):
+        """Validate Aadhar Number format (XXXX XXXX XXXX)."""
         aadhar_number = self.aadhar_number
 
         if not aadhar_number:
             frappe.throw(_("Aadhar number is required."))
 
-        # Single regex check: 4 digits, space, 4 digits, space, 4 digits (not starting with 0 or 1)
         if not re.match(r'^[2-9]\d{3} \d{4} \d{4}$', aadhar_number):
             frappe.throw(_("Aadhar number should have 12 digits in the format 'XXXX XXXX XXXX' and should not start with 0 or 1."))
+
+    def validate_mobile_number(self):
+        """Validate Mobile Number format (10 digits, starts with 6-9)."""
+        mobile_number = self.mobile_number
+
+        if not mobile_number:
+            frappe.throw(_("Mobile number is required."))
+
+        if not re.match(r'^[6789]\d{9}$', mobile_number):
+            frappe.throw(_("Mobile number should be exactly 10 digits and start with 6, 7, 8, or 9."))
