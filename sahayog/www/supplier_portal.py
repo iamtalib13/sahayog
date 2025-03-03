1070052757154849f13087b7b72354678d6d040f
arshadqureshi93 <mo.arshad.93733@gmail.com>
2025-03-03 13:56:26 +0530

    feat:making supplier portal with custom html ++

--- sahayog/www/supplier_portal.py
+++ sahayog/www/supplier_portal.py
@@ -9,3 +9,14 @@ def get_rfq_items(rfq_name):
         fields=["item_code", "qty", "creation"]
     )
     return items
+
+
+@frappe.whitelist()
+def get_sq_items(sq_name):
+    """Fetch Supplier Quotation items dynamically based on the clicked SQ"""
+    items = frappe.get_all(
+        "Supplier Quotation Item",
+        filters={"parent": sq_name},
+        fields=["item_code", "qty", "amount", "base_amount"]
+    )
+    return items
