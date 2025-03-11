d5a998c070e963c5617ddde81cd1f7f2593ddc50
Apeksha-Raut <apeksharaut52@gmail.com>
2025-03-11 17:54:00 +0530

    feat: Added team field in issue register and module for erptech

--- sahayog/issue_register/doctype/issue_register/issue_register.js
+++ sahayog/issue_register/doctype/issue_register/issue_register.js
@@ -1,8 +1,24 @@
 // Copyright (c) 2025, Developer Team and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Issue Register", {
-// 	refresh(frm) {
+frappe.ui.form.on("Issue Register", {
+  refresh: function (frm) {},
 
-// 	},
-// });
+  onload: function (frm) {
+    set_module_query(frm);
+  },
+
+  team: function (frm) {
+    set_module_query(frm); // Update query when team field changes
+  },
+});
+
+function set_module_query(frm) {
+  frm.set_query("module", function () {
+    return {
+      filters: {
+        team: frm.doc.team || "", // Dynamically filter by the team field
+      },
+    };
+  });
+}
