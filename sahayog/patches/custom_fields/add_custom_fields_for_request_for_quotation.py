c9ae1b6f9cdae91b1dc42ea86f0e1a84f34f5d13
Apeksha-Raut <apeksharaut52@gmail.com>
2025-03-18 17:18:59 +0530

    feat: added branch and project and store warehouse and added custom field request for in material_request and RFQ doctype

--- sahayog/patches/custom_fields/add_custom_fields_for_request_for_quotation.py
+++ sahayog/patches/custom_fields/add_custom_fields_for_request_for_quotation.py
@@ -4,15 +4,36 @@ from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
 def execute():
     fields = {
         "Request for Quotation": [
-          {
-                "fieldname": "custom_project",
-                "fieldtype": "Link",
+             {
+                "fieldname": "custom_request_for",
+                "fieldtype": "Select",
                 "insert_after": "billing_address",
                 "reqd": 1,
-                "label": "Project",
-                "options":"Project",
-               
+                "label": "Request For",
+                "options": "\nBranch\nProject\nStore"
             },
+          
+            {
+                "fieldname": "custom_branch",
+                "fieldtype": "Link",
+                "options": "Branch",
+                "insert_after": "custom_request_for",
+                "label": "Branch",
+                "depends_on": "eval:doc.custom_request_for == 'Branch'",
+                "mandatory_depends_on": "eval:doc.custom_request_for == 'Branch'",
+            },
+            {
+                "fieldname": "custom_project",
+                "fieldtype": "Link",
+                "options": "Project",
+                "insert_after": "custom_branch",
+                "label": "Project",
+                "depends_on": "eval:doc.custom_request_for == 'Project'",
+                "mandatory_depends_on": "eval:doc.custom_request_for == 'Project'",
+            }, 
+
+
+           
         ]
     }
 
