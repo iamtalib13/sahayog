5e5bd2365baa5e74ce2b1269787f7c4400024190
arshadqureshi93 <mo.arshad.93733@gmail.com>
2025-05-15 14:09:40 +0530

    fix:custom html block of assigned task

--- sahayog/patches/fixtures/add_custom_html_for_assigned_task.py
+++ sahayog/patches/fixtures/add_custom_html_for_assigned_task.py
@@ -237,6 +237,7 @@ renderAssignedTasks("All");
         doc.script = js_content
         doc.save()
         print("✅ Updated Custom HTML Block: Task Assigned")
+
     else:
         # Create a new Custom HTML Block if it doesn't exist
         frappe.get_doc({
@@ -246,6 +247,7 @@ renderAssignedTasks("All");
             'style': css_content,
             'script': js_content
         }).insert()
+        
         print("✅ Created Custom HTML Block: Task Assigned")
 
     frappe.db.commit()
