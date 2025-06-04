70f8569e76b629303c5cd6168707926315efbc96
iamtalib13 <talibsheikh16@gmail.com>
2025-06-04 15:38:17 +0530

    feat: add Attendee Participation Report for Meeting

---
+++ sahayog/agent_and_bdo/report/attendee_participation_report/attendee_participation_report.js
@@ -0,0 +1,38 @@
+// Copyright (c) 2025, Developer Team and contributors
+// For license information, please see license.txt
+
+frappe.query_reports["Attendee Participation Report"] = {
+  filters: [
+    {
+      fieldname: "from_date",
+      label: "From Date",
+      fieldtype: "Date",
+      default: frappe.datetime.add_days(frappe.datetime.get_today(), -30),
+      reqd: 1,
+    },
+    {
+      fieldname: "to_date",
+      label: "To Date",
+      fieldtype: "Date",
+      default: frappe.datetime.get_today(),
+      reqd: 1,
+    },
+    {
+      fieldname: "type",
+      label: "Type",
+      fieldtype: "Select",
+      options: ["", "Employee", "Agent"],
+    },
+    {
+      fieldname: "participant_id",
+      label: "Employee / Agent ID",
+      fieldtype: "Data",
+    },
+    {
+      fieldname: "trainer",
+      label: "Trainer",
+      fieldtype: "Link",
+      options: "Employee",
+    },
+  ],
+};
