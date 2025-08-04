e2c3ecd11c3994fedfc898ed9ea6e13287dd5fe8
Apeksha-Raut <apeksharaut52@gmail.com>
2025-08-04 15:45:27 +0530

    feat: enhanced mis report list view in custom html block

--- sahayog/mis/doctype/mis_report/mis_report.js
+++ sahayog/mis/doctype/mis_report/mis_report.js
@@ -5,8 +5,14 @@ frappe.ui.form.on("MIS Report", {
   refresh(frm) {},
 
   // Runs every time the doc is saved (new or update)
+  // after_save(frm) {
+  //   // Redirect the user to the MIS Admin workspace
+  //   frappe.set_route("/app/mis-admin");
+  // },
   after_save(frm) {
-    // Redirect the user to the MIS Admin workspace
-    frappe.set_route("/app/mis-admin");
+    // Add small delay to ensure save is complete
+    setTimeout(() => {
+      frappe.set_route("Workspaces", "MIS Admin");
+    }, 500);
   },
 });
