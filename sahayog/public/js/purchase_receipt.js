46bb41b456574d941606b6bc0f62b2072fa0bc1e
Talib Sheikh <talibsheikh16@gmail.com>
2025-09-11 15:14:32 +0530

    feat: hide sidebar in inward list and form

--- sahayog/public/js/purchase_receipt.js
+++ sahayog/public/js/purchase_receipt.js
@@ -1,5 +1,8 @@
 frappe.ui.form.on("Purchase Receipt", {
   refresh: function (frm) {
+    frm.trigger("hide_sidebar");
+
+    var cur_page = frappe.ui.get_cur_page();
     if (cur_page && cur_page.set_title) {
       cur_page.set_title("Inward Form");
     }
@@ -13,5 +16,9 @@ frappe.ui.form.on("Purchase Receipt", {
     if (frm.page) {
       frm.page.set_title("Inward Form");
     }
+  },
+  hide_sidebar: function (frm) {
+    $("span.sidebar-toggle-btn").hide();
+    $(".col-lg-2.layout-side-section").hide();
   },
 });
--- sahayog/public/js/purchase_receipt_list.js
+++ sahayog/public/js/purchase_receipt_list.js
@@ -1,5 +1,7 @@
 frappe.listview_settings["Purchase Receipt"] = {
   onload(listview) {
     listview.page.set_title("Inward List");
+    $("span.sidebar-toggle-btn").hide();
+    $(".col-lg-2.layout-side-section").hide();
   },
 };
