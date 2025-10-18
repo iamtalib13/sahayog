d629be638c311c261a14485d286f95f882fdcefc
Rishabh Rahangdale <rishabhnrahangdale@gmail.com>
2025-10-18 14:53:54 +0530

    feat: auto-set suspension_to_date from suspension_from_date and suspension_days, restrict case_id to set once, and balance field layout across Disciplinary doctypes

--- sahayog/hrms/doctype/suspension_process/suspension_process.js
+++ sahayog/hrms/doctype/suspension_process/suspension_process.js
@@ -1,8 +1,26 @@
 // Copyright (c) 2025, Developer Team and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Suspension Process", {
-// 	refresh(frm) {
+frappe.ui.form.on("Suspension Process", {
+	refresh(frm) {
+        
+	},
+     number_of_days_suspension_required: function(frm) {
+        frm.trigger("calculate_suspension_to_date");
+    },
+    suspenstion_from_date: function(frm) {
+        frm.trigger("calculate_suspension_to_date");
+    },
+    calculate_suspension_to_date: function(frm) {
+        if (frm.doc.number_of_days_suspension_required && frm.doc.suspenstion_from_date) {
+            // Convert date to JS Date object
+            let fromDate = frappe.datetime.str_to_obj(frm.doc.suspenstion_from_date);
+            
+            // Add days
+            let toDate = frappe.datetime.add_days(fromDate, frm.doc.number_of_days_suspension_required);
 
-// 	},
-// });
+            // Set auto to_date
+            frm.set_value("suspenstion_to_date", frappe.datetime.obj_to_str(toDate));
+        }
+    }
+});
