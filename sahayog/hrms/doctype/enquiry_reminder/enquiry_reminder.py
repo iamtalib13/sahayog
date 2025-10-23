a66d14e67ee6a4df738a32c05009edc41a00f29e
Suraiyya Sutriya <suraiyyasutriya2@gmail.com>
2025-10-23 16:35:41 +0530

    feat:Auto-fetch Domestic Enquiry details in Enquiry Reminder DocType in DAMS

--- sahayog/hrms/doctype/enquiry_reminder/enquiry_reminder.py
+++ sahayog/hrms/doctype/enquiry_reminder/enquiry_reminder.py
@@ -1,9 +1,23 @@
-# Copyright (c) 2025, Developer Team and contributors
-# For license information, please see license.txt
-
-# import frappe
+import frappe
 from frappe.model.document import Document
 
-
 class EnquiryReminder(Document):
-	pass
+    def before_insert(self):
+        """Auto-fetch fields from latest Domestic Enquiry for the same case_id"""
+        if self.case_id:
+            de_list = frappe.get_all(
+                "Domestic Enquiry",
+                filters={"case_id": self.case_id},
+                order_by="creation desc",
+                fields=["name", "domestic_enquiry", "status_of_response", "date_of_enquiry",
+                        "place_of_enquiry", "enquiry_officer_name", "remarks"]
+            )
+            if de_list:
+                de = de_list[0]  # latest Domestic Enquiry
+                # ✅ Fetch "Yes" / "No" from Domestic Enquiry field
+                self.domestic_enquiry = de.domestic_enquiry
+                self.status_of_response = de.status_of_response
+                self.date_of_enquiry = de.date_of_enquiry
+                self.place_of_enquiry = de.place_of_enquiry
+                self.enquiry_officer_name = de.enquiry_officer_name
+                self.remarks = de.remarks
