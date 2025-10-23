63fc883149e2faaa1a6d0351a68e39d9f62c91c3
Suraiyya Sutriya <suraiyyasutriya2@gmail.com>
2025-10-23 13:41:20 +0530

    feat:Restrict Domestic Enquiry creation based on “Status of Response” in DAMS using case_id

--- sahayog/hrms/doctype/response_to_scn/response_to_scn.js
+++ sahayog/hrms/doctype/response_to_scn/response_to_scn.js
@@ -1,8 +1,27 @@
 // Copyright (c) 2025, Developer Team and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Response to SCN", {
-// 	refresh(frm) {
+frappe.ui.form.on("Response to SCN", {
+  refresh(frm) {
+    // 🚫 Restrict "+ New Domestic Enquiry" creation when Status of Response = "Satisfactory"
+    setTimeout(() => {
+      const $btn = $('button[data-doctype="Domestic Enquiry"]');
 
-// 	},
-// });
+      // attach namespaced mousedown handler to block form creation
+      $btn.off("mousedown.de_check").on("mousedown.de_check", function (e) {
+        if (frm.doc.status_of_response === "Satisfactory") {
+          e.stopImmediatePropagation();
+          e.preventDefault(); // ✅ fully stops new form opening
+          frappe.msgprint({
+            title: __("Not Allowed"),
+            message: __(
+              "Domestic Enquiry cannot be created because 'Status of Response' is 'Satisfactory'."
+            ),
+            indicator: "red",
+          });
+          return false;
+        }
+      });
+    }, 1000);
+  },
+});
