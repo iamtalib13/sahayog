77ef894b9062c3c8578e3395b113ef8e9ccf4c19
Talib Sheikh <talibsheikh16@gmail.com>
2025-11-07 16:37:40 +0530

    feat(my agent):fix the title of page name.

--- sahayog/sahayog/page/my_agent/my_agent.js
+++ sahayog/sahayog/page/my_agent/my_agent.js
@@ -1,7 +1,7 @@
 frappe.pages["my-agent"].on_page_load = function (wrapper) {
   var page = frappe.ui.make_app_page({
     parent: wrapper,
-    title: "",
+    title: "My Agents",
     single_column: true,
   });
 
