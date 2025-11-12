a9efcc63d65d3a5a571f0b742245ee86ce035238
Rishabh Rahangdale <rishabhnrahangdale@gmail.com>
2025-11-12 11:30:49 +0530

    feat: rename New Case button name to Initiate Case and set default no to subsistance allowance

--- sahayog/hrms/report/disciplinary_cases/disciplinary_cases.js
+++ sahayog/hrms/report/disciplinary_cases/disciplinary_cases.js
@@ -28,10 +28,10 @@ frappe.query_reports["Disciplinary Cases"] = {
     ],
 
     // ---------------------------
-    // On Load (Add Clear Filters & New Case Button)
+    // On Load (Add Clear Filters & New Case Button renamed to Initiate Case)
     // ---------------------------
     onload: function (report) {
-        const btn = report.page.add_inner_button(__('New Case'), () => frappe.new_doc('Disciplinary Case'));
+        const btn = report.page.add_inner_button(__('Initiate Case'), () => frappe.new_doc('Disciplinary Case'));
         $(btn).css({ background: '#000', color: '#fff', borderRadius: '6px', transition: '0.2s',})
 		.hover(
           function () { $(this).css('background', '#444'); },
