c26466ff87ecbb0e48caac8697720e3454ad306f
Suraiyya Sutriya <suraiyyasutriya2@gmail.com>
2025-11-13 11:28:53 +0530

    feat:Added structured autoname logic for Unauthorized Absence and Reminder Of Unauthorized Absence doctypes

--- sahayog/hrms/doctype/unauthorized_absence/unauthorized_absence.py
+++ sahayog/hrms/doctype/unauthorized_absence/unauthorized_absence.py
@@ -1,9 +1,14 @@
 # Copyright (c) 2025, Developer Team and contributors
 # For license information, please see license.txt
-
-# import frappe
+import frappe
 from frappe.model.document import Document
 
-
 class UnauthorizedAbsence(Document):
-	pass
+    def autoname(self):
+        """Generate structured name based on linked Disciplinary Case"""
+        if self.case_id:
+            count = frappe.db.count("Unauthorized Absence", {"case_id": self.case_id}) + 1
+            self.name = f"{self.case_id}-UA-{count:02d}"
+        else:
+            # fallback naming if no case linked
+            self.name = frappe.model.naming.make_autoname("UA-.#####")
