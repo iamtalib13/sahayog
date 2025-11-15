d7204fb56b08778a48a6e41c12b406f49e9f4898
Talib Sheikh <talibsheikh16@gmail.com>
2025-11-15 15:04:07 +0530

    feat:Show stock balance report for all warehouses

--- sahayog/procurement/report/branch_stock/branch_stock.py
+++ sahayog/procurement/report/branch_stock/branch_stock.py
@@ -1,9 +1,36 @@
-# Copyright (c) 2025, Developer Team and contributors
-# For license information, please see license.txt
-
-# import frappe
-
-
+import frappe
 def execute(filters=None):
-	columns, data = [], []
-	return columns, data
+    filters = filters or {}
+
+    columns = [
+        dict(fieldname='item_code', label='Item Code', fieldtype='Link', options='Item', width=150),
+        dict(fieldname='item_name', label='Item Name', fieldtype='Data', width=200),
+        dict(fieldname='warehouse', label='Warehouse', fieldtype='Link', options='Warehouse', width=150),
+        dict(fieldname='stock_balance', label='Stock Balance', fieldtype='Float', width=100),
+    ]
+
+    conditions = []
+    values = {}
+
+    if filters.get('warehouse'):
+        conditions.append('bin.warehouse = %(warehouse)s')
+        values['warehouse'] = filters['warehouse']
+
+    where_clause = 'WHERE ' + ' AND '.join(conditions) if conditions else ''
+
+    query = f'''
+        SELECT
+            bin.item_code,
+            item.item_name,
+            bin.warehouse,
+            SUM(bin.actual_qty) as stock_balance
+        FROM `tabBin` bin
+        LEFT JOIN `tabItem` item ON bin.item_code = item.name
+        {where_clause}
+        GROUP BY bin.item_code, item.item_name, bin.warehouse
+        ORDER BY bin.item_code ASC
+    '''
+
+    data = frappe.db.sql(query, values, as_dict=True)
+
+    return columns, data
