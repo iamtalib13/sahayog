bd5abb7816699bdfc34ae597e1cabc682e5eb095
Suraiyya Sutriya <suraiyyasutriya2@gmail.com>
2025-12-09 13:46:36 +0530

    feat:Added a new Email Template for SCN emails

--- sahayog/hooks.py
+++ sahayog/hooks.py
@@ -548,12 +548,12 @@ fixtures = [
             ]
         ]
     },
-
-    # email templates fixtures
-    {
-        "dt": "Email Template",
-        "filters": [["name", "in", ["Disciplinary Case Update"]]]
-    },  
+# email templates fixtures
+{
+    "dt": "Email Template",
+    "filters": [["name", "in", ["Disciplinary Case Update", "Disciplinary - SCN"]]]
+},
+  
     # Print Format fixture
 {
     "dt": "Print Format",
