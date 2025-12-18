f9cbbd3db270391cf8edef062c20d34c5feeafd0
Shreyash Thantharate <shreyashsunilthantharate@gmail.com>
2025-12-18 17:49:40 +0530

    feat/add-email-tamplete-to-fixtures

--- sahayog/hooks.py
+++ sahayog/hooks.py
@@ -334,6 +334,12 @@ doc_events = {
 # ---------------
 scheduler_events = {
     "cron": {
+        
+        # every day at 18:00 (6 PM)
+                "35 17 * * *": [
+            "sahayog.agent_and_bdo.doctype.agent_activation_call_log.trainer_report.send_daily_trainer_report"
+        ],
+
         # Run daily at 5:00 AM — Early morning department ticket summary email
         "0 5 * * *": [
             "sahayog.templates.emails.notification.send_department_wise_ticket_summary"
@@ -583,7 +589,9 @@ fixtures = [
                                 "Reminder Notice of Enquiry",
                                 "Unauthorized Absence",
                                 "Reminder Of Unauthorized Absence",
-                                "Case Closure Update"]]]
+                                "Case Closure Update",
+                                "Trainer Daily Activity"
+                                ]]]
 },
   
     # Print Format fixture
