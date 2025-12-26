aa19e28432009602e72c88d980e50390608c3e68
Talib Sheikh <talibsheikh16@gmail.com>
2025-12-26 17:22:27 +0530

    feat: add bulk EMMR creation with qty, employee, and warehous.

--- sahayog/procurement/report/branch_stock/branch_stock.py
+++ sahayog/procurement/report/branch_stock/branch_stock.py
@@ -1,4 +1,5 @@
 import frappe
+
 def execute(filters=None):
     filters = filters or {}
 
@@ -7,32 +8,26 @@ def execute(filters=None):
         dict(fieldname='item_name', label='Item Name', fieldtype='Data', width=200),
         dict(fieldname='warehouse', label='Warehouse', fieldtype='Link', options='Warehouse', width=150),
         dict(fieldname='stock_balance', label='Stock Balance', fieldtype='Float', width=100),
-        dict(fieldname="issue",label="Issue",fieldtype="Data",width=80),
-
+        dict(fieldname="issue", label="Issue", fieldtype="Data", width=80),
+        dict(fieldname="inward", label="Inward", fieldtype="Data", width=90),
+        dict(fieldname="select_for_emmr", label="Request", fieldtype="Data", width=60),
     ]
 
-    conditions = []
-    values = {}
-
-    # Only show records for "Stores - S"
-    conditions.append('bin.warehouse = %(warehouse)s')
-    values['warehouse'] = 'Stores - S'
-
-    where_clause = 'WHERE ' + ' AND '.join(conditions) if conditions else ''
-
-    query = f'''
+    query = """
         SELECT
             bin.item_code,
             item.item_name,
             bin.warehouse,
-            SUM(bin.actual_qty) as stock_balance
+            SUM(bin.actual_qty) AS stock_balance,
+            '' AS issue,
+            '' AS receive,
+            '' AS select_for_emmr
         FROM `tabBin` bin
         LEFT JOIN `tabItem` item ON bin.item_code = item.name
-        {where_clause}
+        WHERE bin.warehouse = %(warehouse)s
         GROUP BY bin.item_code, item.item_name, bin.warehouse
         ORDER BY bin.item_code ASC
-    '''
+    """
 
-    data = frappe.db.sql(query, values, as_dict=True)
-
+    data = frappe.db.sql(query, {"warehouse": "Stores - S"}, as_dict=True)
     return columns, data
