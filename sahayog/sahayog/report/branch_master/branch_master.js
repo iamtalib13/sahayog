be12c3ec42b7f4ade6a791e56365e967dd29eee0
Shreyash Thantharate <shreyashsunilthantharate@gmail.com>
2026-01-02 13:34:29 +0530

    Basic Report Structure is Ready

--- sahayog/sahayog/report/branch_master/branch_master.js
+++ sahayog/sahayog/report/branch_master/branch_master.js
@@ -1,8 +1,38 @@
-// Copyright (c) 2026, Developer Team and contributors
-// For license information, please see license.txt
+frappe.query_reports["Branch Master Report"] = {
+    "filters": [
+        {
+            fieldname: "branch_search",
+            label: "Branch / SOL Search",
+            fieldtype: "Autocomplete",
+            reqd: 1,
+            options: [],
+            description: "Type SOL ID or Branch Name to search"
+        }
+    ],
 
-frappe.query_reports["Branch Master"] = {
-	"filters": [
+    onload: function(report) {
+        const filter = report.get_filter("branch_search");
+        filter.$input.on("input", frappe.utils.debounce(function () {
+            let text = $(this).val();
+            if (!text || text.length < 2) {
+                return;
+            }
 
-	]
+            frappe.call({
+                method: "sahayog.sahayog.report.branch_master.branch_master.get_branch_suggestions",
+                args: { text },
+                callback: function(r) {
+                    if (!r.message) return;
+
+                    // build options: "SOLID - Branch Name (District)"
+                    let opts = r.message.map(row => {
+                        return `${row.sol_id} - ${row.branch} (${row.district})`;
+                    });
+
+                    filter.df.options = opts.join("\n");
+                    filter.refresh();
+                }
+            });
+        }, 300));
+    }
 };
