956a71c35345bb3120729006871c53b14c9e6919
Shreyash Thantharate <shreyashsunilthantharate@gmail.com>
2026-01-09 15:54:50 +0530

    Add Branch Petty Cash Account controller

--- sahayog/petty_cash_management/doctype/branch_petty_cash_account/branch_petty_cash_account.py
+++ sahayog/petty_cash_management/doctype/branch_petty_cash_account/branch_petty_cash_account.py
@@ -1,9 +1,22 @@
-# Copyright (c) 2026, Developer Team and contributors
-# For license information, please see license.txt
-
-# import frappe
+import frappe
 from frappe.model.document import Document
 
-
 class BranchPettyCashAccount(Document):
-	pass
+    def before_save(self):
+        # Auto-set monthly limit based on branch type
+        if self.branch_type == "Metro":
+            self.monthly_limit = 25000
+        else:
+            self.monthly_limit = 15000
+    
+    def get_current_balance(self):
+        # Calculate from all transactions
+        balance = frappe.db.sql("""
+            SELECT 
+                COALESCE(SUM(CASE WHEN transaction_type = 'Fund Allocation' THEN amount ELSE 0 END), 0) -
+                COALESCE(SUM(CASE WHEN transaction_type = 'Expense' THEN amount ELSE 0 END), 0)
+            FROM `tabPetty Cash Transaction`
+            WHERE branch = %s AND docstatus = 1
+        """, self.branch)[0][0]
+        
+        return balance or 0
