52feb2c8b352dde5d14217142439d1a234d27a58
Apeksha-Raut <apeksharaut52@gmail.com>
2026-01-09 11:11:10 +0530

    feat: Recreate Report Preference doctype

--- sahayog/sahayog/doctype/report_preference/report_preference.js
+++ sahayog/sahayog/doctype/report_preference/report_preference.js
@@ -1,4 +1,4 @@
-// Copyright (c) 2025, Developer Team and contributors
+// Copyright (c) 2026, Developer Team and contributors
 // For license information, please see license.txt
 
 // frappe.ui.form.on("Report Preference", {
