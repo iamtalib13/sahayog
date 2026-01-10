660ac9f088545f673184ca4e45e64a31e0e7ed02
Rishabh Rahangdale <rishabhnrahangdale@gmail.com>
2026-01-10 11:10:45 +0530

    feat: register Petite-Vue library in hooks for global availability

--- sahayog/hooks.py
+++ sahayog/hooks.py
@@ -73,6 +73,7 @@ doctype_list_js = {
 }
 # app_include_js = "/assets/frappe/js/frappe-web.min.js"
 app_include_js = ["/assets/sahayog/js/assignmate.js"]
+app_include_js = ["/assets/sahayog/js/petite-vue.iife.js",]
 
 
 # doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
