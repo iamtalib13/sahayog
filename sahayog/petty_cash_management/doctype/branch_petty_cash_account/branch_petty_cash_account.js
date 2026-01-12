52010116dc08f3687e0489a536ecd7bfa0490ff0
Shreyash Thantharate <shreyashsunilthantharate@gmail.com>
2026-01-12 13:59:14 +0530

    To show the value of monthly_limit immediately to the user on the screen.

--- sahayog/petty_cash_management/doctype/branch_petty_cash_account/branch_petty_cash_account.js
+++ sahayog/petty_cash_management/doctype/branch_petty_cash_account/branch_petty_cash_account.js
@@ -6,3 +6,25 @@
 
 // 	},
 // });
+
+
+frappe.ui.form.on('Branch Petty Cash Account', {
+    branch: function(frm) {
+        if (frm.doc.branch) {
+            // Fetch the branch_type from the Master
+            frappe.db.get_value('Sahayog Branch', frm.doc.branch, 'branch_type')
+                .then(r => {
+                    if (r && r.message) {
+                        let b_type = r.message.branch_type;
+                        
+                        // Set the limit based on type
+                        if (b_type === "Metro") {
+                            frm.set_value('monthly_limit', 25000);
+                        } else {
+                            frm.set_value('monthly_limit', 15000);
+                        }
+                    }
+                });
+        }
+    }
+});
