4f91ed087961a041115a1adf44c8cbfb2dfec084
Shreyash Thantharate <shreyashsunilthantharate@gmail.com>
2026-01-13 12:33:18 +0530

    Create a new child table Expense Category Item and customise it

--- sahayog/petty_cash_management/doctype/expense_category/expense_category.py
+++ sahayog/petty_cash_management/doctype/expense_category/expense_category.py
@@ -1,9 +1,20 @@
-# Copyright (c) 2026, Developer Team and contributors
-# For license information, please see license.txt
-
-# import frappe
+import frappe
 from frappe.model.document import Document
 
-
 class ExpenseCategory(Document):
-	pass
+
+    def validate(self):
+        self.set_category_name_from_items()
+
+    def set_category_name_from_items(self):
+        category_names = []
+
+        # 🔴 use actual child table fieldname
+        for row in self.table_nezx:
+            if row.category_name:
+                category_names.append(row.category_name.strip())
+
+        # remove duplicates (order preserved)
+        category_names = list(dict.fromkeys(category_names))
+
+        self.category_name = ", ".join(category_names)
