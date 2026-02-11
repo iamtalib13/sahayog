8d98eec5d1aa8f3d2bb00cc49361a32627a683f1
Shreyash Thantharate <shreyashsunilthantharate@gmail.com>
2026-02-11 16:52:25 +0530

    Hide cancel button after verification

--- sahayog/petty_cash_management/doctype/petty_cash_transaction/petty_cash_transaction.js
+++ sahayog/petty_cash_management/doctype/petty_cash_transaction/petty_cash_transaction.js
@@ -191,6 +191,15 @@ frappe.ui.form.on('Petty Cash Transaction', {
                 }, __("Download Files"));
             }
         }
+
+        
+
+         // Hide Cancel button if status is Verified
+        if (frm.doc.approval_status === 'Verified') {
+            frm.page.clear_secondary_action();  // Removes Cancel button
+            // Or use this more specific approach:
+            // frm.page.btn_secondary.hide();
+        }
     },
 
 
