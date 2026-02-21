59bbc77b69c962674a6ab8b11f809ab3c329026e
Suraiyya Sutriya <suraiyyasutriya2@gmail.com>
2026-02-21 11:20:11 +0530

    feat:clean invalid amount_of_fraud before currency migration

--- sahayog/patches/fix_amount_of_fraud.py
+++ sahayog/patches/fix_amount_of_fraud.py
@@ -2,10 +2,16 @@ import frappe
 
 def execute():
     if frappe.db.table_exists("Unauthorized Absence"):
+        # First set NULL values to 0
         frappe.db.sql("""
             UPDATE `tabUnauthorized Absence`
             SET amount_of_fraud = 0
             WHERE amount_of_fraud IS NULL
-               OR amount_of_fraud = ''
-               OR amount_of_fraud NOT REGEXP '^[0-9]+(\\.[0-9]+)?$'
+        """)
+
+        # Force convert any invalid values safely
+        frappe.db.sql("""
+            UPDATE `tabUnauthorized Absence`
+            SET amount_of_fraud = 0
+            WHERE amount_of_fraud + 0 IS NULL
         """)
