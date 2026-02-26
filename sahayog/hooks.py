5fcc81f5e58b88397bbe60b9c04c8e53341f4223
Shreyash Thantharate <shreyashsunilthantharate@gmail.com>
2026-02-26 13:57:56 +0530

    Schedule daily_agent_update_job to run daily at 2 AM

--- sahayog/hooks.py
+++ sahayog/hooks.py
@@ -356,12 +356,7 @@ doc_events = {
 # ---------------
 scheduler_events = {
     "cron": {
-
-        # Run daily at 12:00 AM — Auto Agent updation job to sync with Finacle and create/update agents in Sahayog
-        "0 2 * * *": [
-            "sahayog.api.auto_agent_creation.daily_agent_update_job"
-        ],
-
+        
         # Run daily at 12:00 PM — Branch Petty Cash Account Balance Sync
         # "30 13 * * *": [
         #     "sahayog.petty_cash_management.api.branch_petty_cash_account_balance_fetch.fetch_finacle_balance"
@@ -401,6 +396,12 @@ scheduler_events = {
         "0 1 * * *": [
             "sahayog.api.auto_agent_creation.auto_create_agents_from_scheduler"
         ],
+
+        # Run daily at 1:00 AM — Agent auto-update sync job
+       "0 2 * * *": [  # Runs at 1:00 AM daily
+            "sahayog.api.auto_agent_creation.daily_agent_update_job"
+        ],
+
         # "*/5 * * * *": [
         #     "sahayog.tasks.reset_auto_prepared_reports"  
         # ],
