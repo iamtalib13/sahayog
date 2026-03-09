e188d3f197efc7c760c20862db280e4ec1cb4444
Suraiyya Sutriya <suraiyyasutriya2@gmail.com>
2026-03-09 11:44:44 +0530

    feat:make_customer_details_fields_read_only_when_is_new_customer_checkbox_is_unckecked

--- sahayog/loan/doctype/loan_application/loan_application.js
+++ sahayog/loan/doctype/loan_application/loan_application.js
@@ -1,8 +1,19 @@
-// Copyright (c) 2026, Developer Team and contributors
-// For license information, please see license.txt
+frappe.ui.form.on("Loan Application", {
+  onload: function (frm) {
+    // Form load hote hi check karega
+    frm.trigger("is_new_customer");
+  },
+  refresh: function (frm) {
+    // Form refresh par bhi check karega
+    frm.trigger("is_new_customer");
+  },
+  is_new_customer: function (frm) {
+    // Agar checkbox Checked hai (1) toh fields Editable (read_only = 0)
+    // Agar checkbox Unchecked hai (0) toh fields Locked (read_only = 1)
+    let is_new = frm.doc.is_new_customer ? 0 : 1;
 
-// frappe.ui.form.on("Loan Application", {
-// 	refresh(frm) {
-
-// 	},
-// });
+    frm.set_df_property("customer_name", "read_only", is_new);
+    frm.set_df_property("pan__aadhaar", "read_only", is_new);
+    frm.set_df_property("mobile_number", "read_only", is_new);
+  },
+});
