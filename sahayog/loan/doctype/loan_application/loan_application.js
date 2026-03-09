a64c1c216c55a039035cb7cb8dcb4cd896656f57
Suraiyya Sutriya <suraiyyasutriya2@gmail.com>
2026-03-09 12:57:41 +0530

    feat:added validation and restrict entering numbers in customer name field

--- sahayog/loan/doctype/loan_application/loan_application.js
+++ sahayog/loan/doctype/loan_application/loan_application.js
@@ -5,7 +5,6 @@ frappe.ui.form.on("Loan Application", {
     // Check if field is rendered to avoid 'undefined' error
     if (frm.fields_dict.mobile_number && frm.fields_dict.mobile_number.$input) {
       let mobile_input = frm.fields_dict.mobile_number.$input;
-
       // Unbind previous events to prevent multiple triggers
       mobile_input.off("keypress paste");
 
@@ -32,6 +31,34 @@ frappe.ui.form.on("Loan Application", {
         }, 100);
       });
     }
+    // 2. Customer Name Restriction - Only Alphabets & Spaces
+    if (frm.fields_dict.customer_name && frm.fields_dict.customer_name.$input) {
+      let name_input = frm.fields_dict.customer_name.$input;
+
+      name_input.on("keypress", function (e) {
+        // Allow: A-Z (65-90), a-z (97-122), and Space (32)
+        let charCode = e.which;
+        if (
+          !(charCode >= 65 && charCode <= 90) && // Uppercase
+          !(charCode >= 97 && charCode <= 122) && // Lowercase
+          charCode !== 32 // Space
+        ) {
+          e.preventDefault();
+          return false;
+        }
+      });
+
+      // Paste block to clean numbers from name
+      name_input.on("paste", function (e) {
+        setTimeout(() => {
+          let value = $(this)
+            .val()
+            .replace(/[^a-zA-Z\s]/g, "");
+          $(this).val(value);
+          frm.set_value("customer_name", value);
+        }, 100);
+      });
+    }
   },
 
   is_new_customer: function (frm) {
@@ -48,6 +75,14 @@ frappe.ui.form.on("Loan Application", {
   },
 
   validate: function (frm) {
+    // Server pe save hone se pehle final check
+    if (frm.doc.customer_name && /[^a-zA-Z\s]/.test(frm.doc.customer_name)) {
+      frappe.throw(
+        __("Customer Name should only contain alphabets and spaces."),
+      );
+    }
+
+    // Mobile validation check (Existing)
     if (frm.doc.mobile_number && !/^\d{10}$/.test(frm.doc.mobile_number)) {
       frappe.throw(__("Mobile Number must be exactly 10 digits."));
     }
