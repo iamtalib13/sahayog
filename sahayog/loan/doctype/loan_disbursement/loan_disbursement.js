f43d03484474e8b1faf311b0569b1cc2bf64a52b
Suraiyya Sutriya <suraiyyasutriya2@gmail.com>
2026-03-13 16:13:04 +0530

    feat(loan_disbursement): add UI helper to fetch loan details

--- sahayog/loan/doctype/loan_disbursement/loan_disbursement.js
+++ sahayog/loan/doctype/loan_disbursement/loan_disbursement.js
@@ -1,8 +1,13 @@
-// Copyright (c) 2026, Developer Team and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Loan Disbursement", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Loan Disbursement", {
+  loan_application: function(frm) {
+    if (frm.doc.loan_application) {
+      frappe.db.get_value("Loan Application", frm.doc.loan_application, ["customer_name", "final_payout", "branch_code"], (r) => {
+        if (r) {
+            frm.set_value("customer_name", r.customer_name);
+            frm.set_value("disbursement_amount", r.final_payout);
+            frm.set_value("branch_code", r.branch_code);
+        }
+      });
+    }
+  }
+});
