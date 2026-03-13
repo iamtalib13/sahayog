5bb4f6981ec6d7d76e5b0bbb82bb305179186bf7
Suraiyya Sutriya <suraiyyasutriya2@gmail.com>
2026-03-13 16:13:26 +0530

    feat(loan_repayment): add real-time balance calculation and field fetching

--- sahayog/loan/doctype/loan_repayment/loan_repayment.js
+++ sahayog/loan/doctype/loan_repayment/loan_repayment.js
@@ -1,8 +1,17 @@
-// Copyright (c) 2026, Developer Team and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Loan Repayment", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Loan Repayment", {
+  loan_application: function(frm) {
+    if (frm.doc.loan_application) {
+      frappe.db.get_value("Loan Application", frm.doc.loan_application, ["customer_name", "remaining_balance"], (r) => {
+        if (r) {
+            frm.set_value("customer_name", r.customer_name);
+            frm.set_value("before_balance", r.remaining_balance);
+        }
+      });
+    }
+  },
+  payment_amount: function(frm) {
+    if (frm.doc.before_balance) {
+        frm.set_value("remaining_balance", flt(frm.doc.before_balance) - flt(frm.doc.payment_amount));
+    }
+  }
+});
