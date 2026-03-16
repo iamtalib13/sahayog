91104407ab932729847dc885f5947491d6bdf780
Suraiyya Sutriya <suraiyyasutriya2@gmail.com>
2026-03-16 15:40:34 +0530

    feat(Loan Disbursement): enforce approved application and auto-update status to disbursed

--- sahayog/loan/doctype/loan_disbursement/loan_disbursement.py
+++ sahayog/loan/doctype/loan_disbursement/loan_disbursement.py
@@ -4,26 +4,19 @@ from frappe import _
 
 class LoanDisbursement(Document):
     def validate(self):
-        self.check_application_status()
-
-    def check_application_status(self):
         if self.loan_application:
-            status = frappe.db.get_value("Loan Application", self.loan_application, "status")
-            if status != "Approved":
-                frappe.throw(_("Loan Application {0} must be in 'Approved' status for disbursement. Current status: {1}").format(self.loan_application, status))
+            app_status = frappe.db.get_value("Loan Application", self.loan_application, "status")
+            if app_status != "Approved":
+                frappe.throw(_("Loan Disbursement can only be created for 'Approved' Loan Applications. Current application status is '{0}'.").format(app_status))
 
     def on_submit(self):
-        # Update Parent Application
-        loan_app = frappe.get_doc("Loan Application", self.loan_application)
-        loan_app.status = "Disbursed"
-        # Set initial outstanding balance to the full loan amount
-        loan_app.remaining_balance = loan_app.loan_amount
-        loan_app.save()
-        
-        frappe.msgprint(_("Loan Application {0} marked as Disbursed. Outstanding balance initialized.").format(self.loan_application))
+        if self.loan_application:
+            # Update Loan Application status to Disbursed
+            frappe.db.set_value("Loan Application", self.loan_application, "status", "Disbursed")
+            frappe.msgprint(_("Linked Loan Application {0} status updated to 'Disbursed'.").format(self.loan_application))
 
     def on_cancel(self):
-        loan_app = frappe.get_doc("Loan Application", self.loan_application)
-        loan_app.status = "Approved"
-        loan_app.remaining_balance = 0
-        loan_app.save()
+        if self.loan_application:
+            # Revert Loan Application status to Approved if disbursement is cancelled
+            frappe.db.set_value("Loan Application", self.loan_application, "status", "Approved")
+            frappe.msgprint(_("Linked Loan Application {0} status reverted to 'Approved'.").format(self.loan_application))
