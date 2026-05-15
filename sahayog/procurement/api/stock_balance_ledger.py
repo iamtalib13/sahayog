3f41ff81ed3c33d254c66d43be2453011c9ab7df
Faizan <iamfaijankq@gmail.com>
2026-05-16 01:45:15 +0530

    feat: add searchable Receipt and Supplier filters to Inward popup
    - Fetch latest 500 Purchase Receipt names from backend
    - Implement searchable dropdowns for 'Receipt' and 'Supplier' in Inward filter
    - Add date pickers and status options to Inward filter section

--- sahayog/procurement/api/stock_balance_ledger.py
+++ sahayog/procurement/api/stock_balance_ledger.py
@@ -789,6 +789,7 @@ def get_portal_master_data():
         "warehouses": [w.name for w in frappe.get_all("Warehouse", filters={"disabled": 0})],
         "sahayog_branches": frappe.get_all("Sahayog Branch", fields=["name", "branch", "sol_id"]),
         "emr_names": [r.name for r in frappe.get_all("Employee Material Request", order_by="creation desc", limit=500)],
+        "purchase_receipt_names": [r.name for r in frappe.get_all("Purchase Receipt", order_by="creation desc", limit=500)],
         "suppliers": frappe.get_all("Supplier", fields=["name", "supplier_name"], filters={"disabled": 0}),
         "items": frappe.get_all("Item", fields=["name", "item_name", "stock_uom", "is_fixed_asset", "custom_item_department", "description"], filters={"disabled": 0}),
         "assets_list": frappe.get_all("Asset", fields=["name", "asset_name", "item_code", "item_name", "location", "custodian"], filters={"docstatus": 1}),
