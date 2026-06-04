581b5b25903cd3e907a7452963e8d4a3c649e76c
Faizan <iamfaijankq@gmail.com>
2026-06-04 10:43:27 +0530

    feat:upadte the lable of an relabel_purchase_date_and_add_description_in_asset.

---
+++ sahayog/patches/custom_fields/relabel_purchase_date_and_add_description_in_asset.py
@@ -0,0 +1,22 @@
+import frappe
+from frappe.custom.doctype.custom_field.custom_field import create_custom_field
+
+def execute():
+    # 1. Relabel Purchase Date to Invoice Date
+    if not frappe.db.exists("Property Setter", {"doc_type": "Asset", "field_name": "purchase_date", "property": "label"}):
+        frappe.make_property_setter({
+            "doctype": "Asset",
+            "fieldname": "purchase_date",
+            "property": "label",
+            "value": "Invoice Date",
+            "property_type": "Data"
+        })
+
+    # 2. Add Description field
+    if not frappe.db.exists("Custom Field", {"dt": "Asset", "fieldname": "custom_description"}):
+        create_custom_field("Asset", {
+            "fieldname": "custom_description",
+            "label": "Description",
+            "fieldtype": "Small Text",
+            "insert_after": "asset_name"
+        })
