7e1e157fababef57c83de78df62a55fb0ca3f488
iamtalib13 <talibsheikh16@gmail.com>
2026-06-20 12:38:16 +0530

    feat(branch): add freeze message on Sync Finacle Branch button

--- sahayog/sahayog/doctype/sahayog_branch/sahayog_branch_list.js
+++ sahayog/sahayog/doctype/sahayog_branch/sahayog_branch_list.js
@@ -3,5 +3,16 @@ frappe.listview_settings["Sahayog Branch"] = {
     // Hide sidebar elements
     $(".layout-side-section").hide();
     $(".sidebar-toggle-btn").hide();
+
+    listview.page.add_inner_button(__("Sync Finacle Branch"), () => {
+      frappe.call({
+        method: "sahayog.sahayog.doctype.sahayog_branch.sahayog_branch.auto_create_sahayog_branches_from_finacle",
+        freeze: true,
+        freeze_message: __("Branch update is in progress. Please wait..."),
+        callback(r) {
+          if (!r.exc) listview.refresh();
+        },
+      });
+    });
   },
 };
