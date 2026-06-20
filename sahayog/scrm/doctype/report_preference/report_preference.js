4eebce468d93a7b6cb4f9fbcbd9961b694f9e60f
Suraiyya Sutriya <suraiyyasutriya2@gmail.com>
2026-06-20 10:46:40 +0530

    feat:added delete option to delete all the assign sol ids in report preference

--- sahayog/scrm/doctype/report_preference/report_preference.js
+++ sahayog/scrm/doctype/report_preference/report_preference.js
@@ -1,58 +1,7 @@
 frappe.ui.form.on("Report Preference", {
   refresh: function (frm) {
     frm.trigger("toggle_region_mandatory");
-  },
 
-  all_regions: function (frm) {
-    frm.trigger("toggle_region_mandatory");
-  },
-
-  zone_add: function (frm) {
-    frm.trigger("toggle_region_mandatory");
-  },
-
-  zone_remove: function (frm) {
-    frm.trigger("toggle_region_mandatory");
-  },
-
-  region_add: function (frm) {
-    frm.trigger("toggle_region_mandatory");
-  },
-
-  region_remove: function (frm) {
-    frm.trigger("toggle_region_mandatory");
-  }, // ✅ comma added here
-
-  // toggle_region_mandatory: function (frm) {
-  //   let zone_selected = frm.doc.zone && frm.doc.zone.length > 0;
-  //   let is_mandatory = zone_selected && !frm.doc.all_regions;
-
-  //   if (frm.fields_dict.region.grid) {
-  //     frm.fields_dict.region.grid.set_column_disp(
-  //       "region",
-  //       "reqd",
-  //       is_mandatory,
-  //     );
-  //   } else {
-  //     frm.set_df_property("region", "reqd", is_mandatory);
-  //   }
-  // },
-
-  enable_preferences: function (frm) {
-    if (!frm.doc.enable_preferences) {
-      // Clear child tables
-      frm.clear_table("product");
-      frm.clear_table("source");
-      frm.clear_table("zone");
-      frm.clear_table("region");
-      frm.clear_table("sol_id");
-
-      frm.refresh_fields();
-
-      frappe.msgprint("All preferences cleared.");
-    }
-  },
-  refresh: function (frm) {
     if (frm.is_new()) {
       frm.add_custom_button("Search User", function () {
         let d = new frappe.ui.Dialog({
@@ -121,6 +70,66 @@ frappe.ui.form.on("Report Preference", {
 
         d.show();
       });
+    }
+
+    // Add "Clear" button to clear already set sol_id
+    frm.add_custom_button(__("Clear"), function () {
+      frm.clear_table("sol_id");
+      frm.refresh_field("sol_id");
+      frappe.show_alert({
+        message: __("SOL IDs cleared."),
+        indicator: "green",
+      });
+    });
+  },
+
+  all_regions: function (frm) {
+    frm.trigger("toggle_region_mandatory");
+  },
+
+  zone_add: function (frm) {
+    frm.trigger("toggle_region_mandatory");
+  },
+
+  zone_remove: function (frm) {
+    frm.trigger("toggle_region_mandatory");
+  },
+
+  region_add: function (frm) {
+    frm.trigger("toggle_region_mandatory");
+  },
+
+  region_remove: function (frm) {
+    frm.trigger("toggle_region_mandatory");
+  }, // ✅ comma added here
+
+  // toggle_region_mandatory: function (frm) {
+  //   let zone_selected = frm.doc.zone && frm.doc.zone.length > 0;
+  //   let is_mandatory = zone_selected && !frm.doc.all_regions;
+
+  //   if (frm.fields_dict.region.grid) {
+  //     frm.fields_dict.region.grid.set_column_disp(
+  //       "region",
+  //       "reqd",
+  //       is_mandatory,
+  //     );
+  //   } else {
+  //     frm.set_df_property("region", "reqd", is_mandatory);
+  //   }
+  // },
+
+  enable_preferences: function (frm) {
+    if (!frm.doc.enable_preferences) {
+      // Clear child tables
+      frm.clear_table("product");
+      frm.clear_table("source");
+      frm.clear_table("zone");
+      frm.clear_table("region");
+      frm.clear_table("sol_id");
+
+      frm.refresh_fields();
+
+      frappe.msgprint("All preferences cleared.");
     }
   },
 });
