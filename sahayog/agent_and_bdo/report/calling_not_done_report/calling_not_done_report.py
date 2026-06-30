240927cb5f2cfd63675afc3c1f39faa74f5da0e8
Suraiyya Sutriya <suraiyyasutriya2@gmail.com>
2026-06-30 16:01:16 +0530

    chore: remove Calling Status column from calling not done report

--- sahayog/agent_and_bdo/report/calling_not_done_report/calling_not_done_report.py
+++ sahayog/agent_and_bdo/report/calling_not_done_report/calling_not_done_report.py
@@ -15,7 +15,6 @@ def execute(filters=None):
         {"label": "Branch Name", "fieldname": "branch_name", "fieldtype": "Data", "width": 160},
         {"label": "Calling Attempts (All Time)", "fieldname": "calling_attempts", "fieldtype": "Int", "width": 160},
         {"label": "Last Call Date", "fieldname": "last_call_date", "fieldtype": "Date", "width": 120},
-        {"label": "Calling Status", "fieldname": "calling_status", "fieldtype": "Data", "width": 110},
     ]
 
     trainer_condition = ""
@@ -31,8 +30,7 @@ def execute(filters=None):
             a.branch_code,
             a.branch_name,
             COUNT(acl.name) AS calling_attempts,
-            MAX(acl.calling_date) AS last_call_date,
-            'Inactive' AS calling_status
+            MAX(acl.calling_date) AS last_call_date
         FROM `tabAgent Activation Call Log` acl
         INNER JOIN `tabAgent` a ON a.name = acl.agent
         LEFT JOIN `tabUser` u ON u.name = acl.trainer
