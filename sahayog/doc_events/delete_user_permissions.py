e81119d3fb2bc633a768a5e72a95e7629284b044
iamtalib13 <talibsheikh16@gmail.com>
2026-07-23 11:10:26 +0530

    fix(doc_events): remove msgprint notification on user permission deletion

--- sahayog/doc_events/delete_user_permissions.py
+++ sahayog/doc_events/delete_user_permissions.py
@@ -1,5 +1,6 @@
 import frappe
 
+
 def delete_user_permissions(doc, method):
     """Delete User Permission for Employee on User save/update"""
 
@@ -9,9 +10,3 @@ def delete_user_permissions(doc, method):
         "allow": "Employee"
     })
 
-    # Inform the user
-    frappe.msgprint(
-        msg=f"Employee User Permissions for <b>{doc.name}</b> have been deleted.",
-        title="User Permission Cleared",
-        indicator="green"
-    )
