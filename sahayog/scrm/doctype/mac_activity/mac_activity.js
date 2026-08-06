fec421b38fb42deedaa4b98af9c190116b239a5c
Rishabh Rahangdale <rishabhnrahangdale@gmail.com>
2026-08-06 12:36:21 +0530

    feat: Auto-fill and lock employee and branch for Desk form users

--- sahayog/scrm/doctype/mac_activity/mac_activity.js
+++ sahayog/scrm/doctype/mac_activity/mac_activity.js
@@ -2,8 +2,49 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('MAC Activity', {
+	onload: function(frm) {
+		if (frm.is_new() && frappe.session.user !== 'Administrator') {
+			frappe.call({
+				method: 'sahayog.scrm.doctype.mac_activity.mac_activity.get_logged_in_employee_details',
+				callback: function(r) {
+					if (r.message && r.message.employee_id) {
+						frm.set_value('employee', r.message.employee_id);
+						if (r.message.sahayog_branch) {
+							frm.set_value('branch', r.message.sahayog_branch);
+						}
+						// Disable editing for non-admins
+						const is_admin = frappe.user_roles.includes('System Manager') || frappe.session.user === 'Administrator';
+						if (!is_admin) {
+							frm.set_df_property('employee', 'read_only', 1);
+							frm.set_df_property('branch', 'read_only', 1);
+						}
+					}
+				}
+			});
+		}
+	},
 	refresh: function(frm) {
 		frm.trigger('toggle_remark_mandatory');
+		const is_admin = frappe.user_roles.includes('System Manager') || frappe.session.user === 'Administrator';
+		if (!frm.is_new() || !is_admin) {
+			frm.set_df_property('employee', 'read_only', 1);
+			frm.set_df_property('branch', 'read_only', 1);
+		}
+	},
+	employee: function(frm) {
+		if (frm.doc.employee) {
+			frappe.call({
+				method: 'sahayog.scrm.doctype.mac_activity.mac_activity.get_employee_details',
+				args: {
+					employee: frm.doc.employee
+				},
+				callback: function(r) {
+					if (r.message && r.message.sahayog_branch) {
+						frm.set_value('branch', r.message.sahayog_branch);
+					}
+				}
+			});
+		}
 	},
 	status: function(frm) {
 		frm.trigger('toggle_remark_mandatory');
