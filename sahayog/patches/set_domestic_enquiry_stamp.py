bd9373a6f34f200e07ac20fa4bf1963caa4c432f
Suraiyya Sutriya <suraiyyasutriya2@gmail.com>
2026-09-02 12:55:16 +0530

    feat: add patch to set domestic_enquiry_stamp in HR Setting

--- sahayog/patches/set_domestic_enquiry_stamp.py
+++ sahayog/patches/set_domestic_enquiry_stamp.py
@@ -1,11 +1,7 @@
 import frappe
 
 def execute():
-    # Set domestic_enquiry_stamp to stamp-2.png for production
-    frappe.db.set_single_value(
-        "Sahayog HR Setting",
-        "domestic_enquiry_stamp",
-        "/assets/sahayog/images/stamp-2.png"
-    )
+    frappe.db.set_single_value("Sahayog HR Setting", "domestic_enquiry_stamp", "/assets/sahayog/images/stamp-2.png")
     frappe.db.commit()
-    print("Set domestic_enquiry_stamp to /assets/sahayog/images/stamp-2.png")
+    val = frappe.db.get_single_value("Sahayog HR Setting", "domestic_enquiry_stamp")
+    print("domestic_enquiry_stamp =", val)
