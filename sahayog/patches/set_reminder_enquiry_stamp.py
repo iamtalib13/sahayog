3b1e7616b6d002e3b8448b3c72b0eba9c74dfc39
Suraiyya Sutriya <suraiyyasutriya2@gmail.com>
2026-09-02 15:19:07 +0530

    chore: update helper patch

--- sahayog/patches/set_reminder_enquiry_stamp.py
+++ sahayog/patches/set_reminder_enquiry_stamp.py
@@ -1,18 +1,18 @@
 import frappe, json
 
 def execute():
-    path = '/home/suraiyya/frappe-bench/apps/sahayog/sahayog/hrms/print_format/ex_parte_enquiry/ex_parte_enquiry.json'
+    path = '/home/suraiyya/frappe-bench/apps/sahayog/sahayog/hrms/print_format/office_order_termination_of_services/office_order_termination_of_services.json'
     with open(path) as f:
         d = json.load(f)
     
-    frappe.db.set_value('Print Format', 'Ex Parte Enquiry', 'css', d['css'])
-    frappe.db.set_value('Print Format', 'Ex Parte Enquiry', 'html', d['html'])
+    frappe.db.set_value('Print Format', 'Office Order Termination of Services', 'css', d['css'])
+    frappe.db.set_value('Print Format', 'Office Order Termination of Services', 'html', d['html'])
     
     fix_path = '/home/suraiyya/frappe-bench/apps/sahayog/sahayog/fixtures/print_format.json'
     with open(fix_path) as f:
         fix_data = json.load(f)
     for item in fix_data:
-        if item['name'] == 'Ex Parte Enquiry':
+        if item['name'] == 'Office Order Termination of Services':
             item['css'] = d['css']
             item['html'] = d['html']
             break
