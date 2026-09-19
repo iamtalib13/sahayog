7cba356fad3b590708b62016e7d6c054bf8462f2
Rishabh Rahangdale <rishabhnrahangdale@gmail.com>
2026-09-19 13:53:54 +0530

    feat(edr-report): set default today date in from and to filters

--- sahayog/sahayog/report/edr_report/edr_report.js
+++ sahayog/sahayog/report/edr_report/edr_report.js
@@ -1,7 +1,7 @@
 frappe.query_reports["EDR Report"] = {
 	"filters": [
-		{"fieldname": "from_date", "label": __("From Date"), "fieldtype": "Date"},
-		{"fieldname": "to_date", "label": __("To Date"), "fieldtype": "Date"},
+		{"fieldname": "from_date", "label": __("From Date"), "fieldtype": "Date", "default": frappe.datetime.get_today()},
+		{"fieldname": "to_date", "label": __("To Date"), "fieldtype": "Date", "default": frappe.datetime.get_today()},
 		{"fieldname": "zone", "label": __("Zone"), "fieldtype": "Data"},
 		{"fieldname": "branch", "label": __("Branch"), "fieldtype": "Link", "options": "Sahayog Branch"},
 		{"fieldname": "designation", "label": __("Designation"), "fieldtype": "Link", "options": "Designation"}
