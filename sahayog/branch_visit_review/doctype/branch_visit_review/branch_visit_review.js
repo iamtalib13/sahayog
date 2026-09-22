7ff3418bae6bd187fcc5ac46ac32db277e47a733
Faijan K <iamfaijankq@gmail.com>
2026-09-22 11:50:13 +0530

    Add branch filter to find Branch Manager employee by sol_id

--- sahayog/branch_visit_review/doctype/branch_visit_review/branch_visit_review.js
+++ sahayog/branch_visit_review/doctype/branch_visit_review/branch_visit_review.js
@@ -1,8 +1,38 @@
 // Copyright (c) 2026, Developer Team and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Branch Visit Review", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Branch Visit Review", {
+	branch(frm) {
+		if (frm.doc.branch) {
+			frappe.call({
+				method: "frappe.client.get_value",
+				args: {
+					doctype: "Sahayog Branch",
+					filters: { name: frm.doc.branch },
+					fieldname: "sol_id",
+				},
+				callback: function (r) {
+					if (r.message && r.message.sol_id) {
+						frappe.call({
+							method: "frappe.client.get_list",
+							args: {
+								doctype: "Employee",
+								filters: {
+									sol_id: r.message.sol_id,
+									designation: "BRANCH MANAGER",
+								},
+								fields: ["name", "employee_name", "sol_id", "designation"],
+								limit_page_length: 0,
+							},
+							callback: function (r) {
+								if (r.message) {
+									console.log("Branch Manager:", r.message);
+								}
+							},
+						});
+					}
+				},
+			});
+		}
+	},
+});
